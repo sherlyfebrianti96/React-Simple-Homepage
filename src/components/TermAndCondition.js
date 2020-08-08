0212f733bc9601ebd7e1ba3cf150e9d774d1c2ba
Sherly Febrianti <sherly.febrianti@allianz.co.id>
2020-08-08 22:10:49 +0700

    Refactoring the TermAndCondition

--- src/components/TermAndCondition.js
+++ src/components/TermAndCondition.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './../styles/TermAndConditionStyle.scss';
 
-export class TermAndCondition extends React.Component {
-	render() {
-		return (
-			<div className="Content Privacy-policy">
-				<div className="Row">
-					<p className="Privacy-policy-text">
-            By accessing and using this website, you acknowledge that you have read and understand
-            our <a href="#">Cookie Policy</a>, <a href="#">Privacy Policy</a>, and our <a href="#">Terms of Service.</a>
-          </p>
-					<div className="Privacy-policy-button">
-						<button onClick={this.props.handleCloseTermAndCondition}>Got it</button>
-					</div>
-					<div className="Clearfix" />
-				</div>
-			</div>
-		);
-	}
+export const TermAndCondition = (props) => {
+  return (
+    <div className="Content Privacy-policy">
+      <div className="Row">
+        <p className="Privacy-policy-text">
+          By accessing and using this website, you acknowledge that you have read and understand
+          our <a href="#">Cookie Policy</a>, <a href="#">Privacy Policy</a>, and our <a href="#">Terms of Service.</a>
+        </p>
+        <div className="Privacy-policy-button">
+          <button onClick={props.handleCloseTermAndCondition}>Got it</button>
+        </div>
+        <div className="Clearfix" />
+      </div>
+    </div>
+  );
 }
