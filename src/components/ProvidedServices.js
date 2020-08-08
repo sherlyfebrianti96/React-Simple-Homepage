f7e7b79dfa54b4c129d32e362c89bd830403049b
Sherly Febrianti <sherly.febrianti@allianz.co.id>
2020-08-09 00:44:42 +0700

    Refactoring the Provided Services

--- src/components/ProvidedServices.js
+++ src/components/ProvidedServices.js
@@ -2,24 +2,18 @@ import React from 'react';
 import {TermAndCondition} from './../components/TermAndCondition';
 import './../styles/ProvidedServices.scss';
 
-export class ProvidedServices extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="Provided-services">
-        <div className="Content">
-          <div className="Row Highlight">
-            <h1>How Can I Help You?</h1>
-            <p>Our work then targeted, best practices outcomes social innovation synergy.</p>
-            <p>Venture philanthropy, revolutionary inclusive policymaker relief. User-centered program areas scale.</p>
-          </div>
-        </div>
-        {this.props.getServices}
-        <div className="Clearfix"></div>
-      </div>
-    );
-  }
+export const ProvidedServices = (props) => {
+	return (
+		<div className="Provided-services">
+			<div className="Content">
+				<div className="Row Highlight">
+					<h1>How Can I Help You?</h1>
+					<p>Our work then targeted, best practices outcomes social innovation synergy.</p>
+					<p>Venture philanthropy, revolutionary inclusive policymaker relief. User-centered program areas scale.</p>
+				</div>
+			</div>
+			{props.getServices}
+			<div className="Clearfix"></div>
+		</div>
+	);
 }
