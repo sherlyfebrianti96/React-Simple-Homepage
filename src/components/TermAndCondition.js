abdfb2e23978747d86c6b644d348640ac02afd21
Sherly Febrianti <sherly.febrianti@allianz.co.id>
2020-08-08 22:04:14 +0700

    Enhance Term and Conditions

--- src/components/TermAndCondition.js
+++ src/components/TermAndCondition.js
@@ -6,8 +6,10 @@ export class TermAndCondition extends React.Component {
 		return (
 			<div className="Content Privacy-policy">
 				<div className="Row">
-					<p className="Privacy-policy-text">By accessing and using this website, you acknowledge that you have read and
-						understand our Cookie Policy, Privacy Policy, and our Terms of Service.</p>
+					<p className="Privacy-policy-text">
+            By accessing and using this website, you acknowledge that you have read and understand
+            our <a href="#">Cookie Policy</a>, <a href="#">Privacy Policy</a>, and our <a href="#">Terms of Service.</a>
+          </p>
 					<div className="Privacy-policy-button">
 						<button onClick={this.props.handleCloseTermAndCondition}>Got it</button>
 					</div>
