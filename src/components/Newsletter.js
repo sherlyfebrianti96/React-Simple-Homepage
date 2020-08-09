18282bc026d3a7494121b70ef8a52737129a4035
Sherly Febrianti <sherly.febrianti@allianz.co.id>
2020-08-09 12:07:53 +0700

    Make the window sizing match the requirement

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -3,23 +3,25 @@ import '../styles/NewsletterStyle.scss';
 
 export const Newsletter = (props) => {
 	return (
-		<div className="Newsletter">
-			<div className="Headline">
-				<h2>Get latest updates in web technologies</h2>
-				<p>
-					I write articles related to web technologies, such as design trends, development tools, UI/UX case studies and
-					reviews, and more. Sign up to my newsletter to get them all.
-				</p>
+		<div className="Newsletter-wrapper">
+			<div className="Newsletter">
+				<div className="Headline">
+					<h2>Get latest updates in web technologies</h2>
+					<p>
+						I write articles related to web technologies, such as design trends, development tools, UI/UX case studies
+						and reviews, and more. Sign up to my newsletter to get them all.
+					</p>
+				</div>
+				<form className="Newsletter-form">
+					<div className="Input">
+						<input type="email" placeholder="Email address"/>
+					</div>
+					<div className="Button">
+						<button>Count me in!</button>
+					</div>
+				</form>
+				<div className="Newsletter-close" onClick={props.handleCloseNewsletter}>X</div>
 			</div>
-			<form className="Newsletter-form">
-				<div className="Input">
-					<input type="email" placeholder="Email address"/>
-				</div>
-				<div className="Button">
-					<button>Count me in!</button>
-				</div>
-			</form>
-			<div className="Newsletter-close" onClick={props.handleCloseNewsletter}>X</div>
 		</div>
 	);
 };
