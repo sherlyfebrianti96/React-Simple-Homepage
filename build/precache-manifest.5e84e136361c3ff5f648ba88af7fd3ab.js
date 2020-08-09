13e30a1fb7fb455d3837375ce0cfd7593a2b1d84
Sherly Febrianti <sherly.febrianti@allianz.co.id>
2020-08-09 23:08:33 +0700

    Save the build result

---
+++ build/precache-manifest.5e84e136361c3ff5f648ba88af7fd3ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c10a23577d78da1637780abde10a2c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e292f07c43429af6d499",
+    "url": "/static/css/main.e5e5f378.chunk.css"
+  },
+  {
+    "revision": "9deccedcb3c60a43f6f4",
+    "url": "/static/js/2.3db797c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3db797c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e292f07c43429af6d499",
+    "url": "/static/js/main.02462ced.chunk.js"
+  },
+  {
+    "revision": "109533a62e4248b7edeb",
+    "url": "/static/js/runtime-main.cd044454.js"
+  },
+  {
+    "revision": "a5a74b4ff9be5b81b4ee10c0f456d8e9",
+    "url": "/static/media/work-desk__dustin-lee.a5a74b4f.jpg"
+  }
+]);
