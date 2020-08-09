00a4e38657e28159eb91428c3fc8de822e492dfa
Sherly Febrianti <sherly.febrianti@allianz.co.id>
2020-08-09 14:06:04 +0700

    Update Newsletter Panel animation

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import '../styles/NewsletterStyle.scss';
 
-export const Newsletter = (props) => {
-	return (
-		<div className="Newsletter-wrapper">
-			<div className="Newsletter">
-				<div className="Headline">
-					<h2>Get latest updates in web technologies</h2>
-					<p>
-						I write articles related to web technologies, such as design trends, development tools, UI/UX case studies
-						and reviews, and more. Sign up to my newsletter to get them all.
-					</p>
+export class Newsletter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			closePanelClicked: false
+		};
+		this.handleOnClick = this.handleOnClick.bind(this);
+	}
+
+	handleOnClick() {
+		this.setState({
+			closePanelClicked: true
+		});
+		this.props.handleCloseNewsletter();
+	}
+
+	getWrapperClass() {
+		let wrapperClass = 'Newsletter-wrapper';
+		if (this.state.closePanelClicked) {
+			wrapperClass += ' Newsletter-slide-down';
+		}
+
+		return wrapperClass;
+	}
+
+	render() {
+		const styles = {
+			bottom: this.props.position
+		};
+
+		return (
+			<div className={this.getWrapperClass()} style={styles}>
+				<div className="Newsletter">
+					<div className="Headline">
+						<h2>Get latest updates in web technologies</h2>
+						<p>
+							I write articles related to web technologies, such as design trends, development tools, UI/UX case studies
+							and reviews, and more. Sign up to my newsletter to get them all.
+						</p>
+					</div>
+					<form className="Newsletter-form">
+						<div className="Input">
+							<input type="email" placeholder="Email address"/>
+						</div>
+						<div className="Button">
+							<button>Count me in!</button>
+						</div>
+					</form>
+					<div className="Newsletter-close" onClick={this.handleOnClick}>X</div>
 				</div>
-				<form className="Newsletter-form">
-					<div className="Input">
-						<input type="email" placeholder="Email address"/>
-					</div>
-					<div className="Button">
-						<button>Count me in!</button>
-					</div>
-				</form>
-				<div className="Newsletter-close" onClick={props.handleCloseNewsletter}>X</div>
 			</div>
-		</div>
-	);
+		);
+	}
 };
--- src/containers/NewsletterContainer.js
+++ src/containers/NewsletterContainer.js
@@ -5,18 +5,23 @@ export class NewsletterContainer extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			showNewsletter: true
+			showNewsletter: true,
+			position: 0
 		};
 		this.handleCloseNewsletter = this.handleCloseNewsletter.bind(this);
 	}
 
 	handleCloseNewsletter() {
-		this.setState({
-			showNewsletter: false
-		});
+		console.log('close clicked');
+		setTimeout(() => {
+			this.setState({
+				showNewsletter: false
+			});
+		}, 5000);
 	}
 
 	render() {
-		return this.state.showNewsletter && <Newsletter handleCloseNewsletter={this.handleCloseNewsletter} />;
+		return this.state.showNewsletter &&
+			<Newsletter position={this.state.position} handleCloseNewsletter={this.handleCloseNewsletter}/>;
 	}
 }
